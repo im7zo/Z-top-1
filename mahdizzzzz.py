e277f7ce5a2007be39559fdaea30ab445de64bea
im7zo <rtgpoibli@gmail.com>
2025-08-06 02:32:08 +0300

    Delete mahdizzzzz.py

--- mahdizzzzz.py
+++
@@ -1,9 +0,0 @@
-from telethon import events
-from config import client  # استيراد الكلاينت من config
-
-@client.on(events.NewMessage(pattern=r"\.تجربة التحديث"))
-async def commands_menu(event):
-    await event.respond(
-        "✅ التحديث يعمل بنجاح!",
-        link_preview=False
-    )
